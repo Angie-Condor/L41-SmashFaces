6afacc42f412ad4d9cb0daa10c2a5bd80badfeb3
Angie Condor <angiegita1602@gmail.com>
2017-06-09 15:57:19 -0500

    avance con html y un poco css

---
+++ js/js-nombres/mexico.js
@@ -0,0 +1,103 @@
+const mexico = [{
+              image: "fotos/mexico/Alma Patricia Jimenez.JPG",
+              name: "Alma"
+            },{
+              image: "fotos/mexico/Ana Maria Barbosa.JPG",
+              name: "Ana"
+            },{
+              image: "fotos/mexico/Analy Miranda.JPG",
+              name: "Analy"  
+            },{
+              image: "fotos/mexico/Areli Rodriguez.JPG",
+              name: "Areli"  
+            },{
+              image: "fotos/mexico/Beatriz Quesadas.JPG",
+              name: "Beatriz"
+            },{
+              image: "fotos/mexico/Claudia Hernández.JPG",
+              name: "Claudia"
+            },{
+              image: "fotos/mexico/Daniela Belem García.JPG",
+              name: "Daniela"
+            },{
+              image: "fotos/mexico/Elisa Guadalupe Martínez.JPG",
+              name: "Elisa"
+            },{
+              image: "fotos/mexico/Evelyn Vázquez.JPG",
+              name: "Evelyn"
+            },{
+              image: "fotos/mexico/Gabriela Peñaloza.JPG",
+              name: "Gabriela"
+            },{
+              image: "fotos/mexico/Grissel Coutiño.JPG",
+              name: "Grissel"
+            },{
+              image: "fotos/mexico/Guadalupe Guerrero.JPG",
+              name: "Guadalupe"
+            },{
+              image: "fotos/mexico/Johana Alexa Vargas.JPG",
+              name: "Johana"
+            },{
+              image: "fotos/mexico/Joyce Zeltzin Hernández.JPG",
+              name: "Joyce"
+            },{
+              image: "fotos/mexico/Juana Ofelia García.JPG",
+              name: "Juana"
+            },{
+              image: "fotos/mexico/Karen A. Sharon De Diego.JPG",
+              name: "Karen"
+            },{
+              image: "fotos/mexico/Karen Cruz Heredia.JPG",
+              name: "Karen Cruz"
+            },{
+              image: "fotos/mexico/Karen Quiroz.JPG",
+              name: "Karen Quiroz"
+            },{
+              image: "fotos/mexico/Karla Monica Llerenas.JPG",
+              name: "Karla"
+            },{
+              image: "fotos/mexico/Karla Vargas.JPG",
+              name: "Karla Vargas"
+            },{
+              image: "fotos/mexico/Leslie Anigail Vazquez.JPG",
+              name: "Leslie"
+            },{
+              image: "fotos/mexico/Lilian Mishel Martínez.JPG",
+              name: "Lilian"
+            },{
+              image: "fotos/mexico/Milca Sarai Del Angel.JPG",
+              name: "Milca"
+            },{
+              image: "fotos/mexico/Naibit Leonel.JPG",
+              name: "Naibit"
+            },{
+              image: "fotos/mexico/Nayeli Carbajal.JPG",
+              name: "Nayeli"
+            },{
+              image: "fotos/mexico/Nelly Montserrat Saavedra.JPG",
+              name: "Nelly"
+            },{
+              image: "fotos/mexico/Reyna Hernández.JPG",
+              name: "Reyna"
+            },{
+              image: "fotos/mexico/Rubí Adriana Santiago.JPG",
+              name: "Rubi"
+            },{
+              image: "fotos/mexico/Ruth López.JPG",
+              name: "Ruth"
+            },{
+              image: "fotos/mexico/Ruth Zacnicte Vega.JPG",
+              name: "Ruth Zacnicte"
+            },{
+              image: "fotos/mexico/Sandra Bollo.JPG",
+              name: "Sandra"
+            },{
+              image: "fotos/mexico/Sandra Díaz.JPG",
+              name: "Sandra Díaz"
+            },{
+              image: "fotos/mexico/Vianey Tavatha Moreno.JPG",
+              name: "Vianey"
+            },{
+              image: "fotos/mexico/Zazil Aurora Martinez.JPG",
+              name: "Zazil"
+            }];
---
+++ js/js-nombres/peru.js
@@ -0,0 +1,124 @@
+const peru =  [{
+              image:"fotos/peru/Ana Paula Verme.jpg",
+              name:"Ana"
+            },{
+              image:"fotos/peru/Joselyn Inga.JPG",
+              name:"Joselyn"
+            },{
+              image:"fotos/peru/Antonella Gutierrez.jpg",
+              name:"Antonella" 
+            },{
+              image:"fotos/peru/Karen Valdivia.JPG",
+              name:"Karen"
+            },{
+              image:"fotos/peru/Astrid Cordova.jpg",
+              name:"Astrid"
+            },{
+              image:"fotos/peru/Carmenrosa Arias.jpg",
+              name:"Carmenrosa"
+            },{
+              image:"fotos/peru/Daniela Segura.JPG",
+              name:"Daniela"
+            },{
+              image:"fotos/peru/Diana Santisteban.JPG",
+              name:"Diana"
+            },{
+              image:"fotos/peru/Emily Elena Vivanco.jpg",
+              name:"Emily"
+            },{
+              image:"fotos/peru/Gabriela Segura.jpg",
+              name:"Gabriela"
+            },{
+              image:"fotos/peru/Genesis Leon.jpg",
+              name:"Genesis"
+            },{
+              image:"fotos/peru/Ingrid Dariana Patiño.jpg",
+              name:"Dariana"
+            },{
+              image:"fotos/peru/Janet Huacahuasi.jpg",
+              name:"Janet"
+            },{
+              image:"fotos/peru/Jenny Vega.JPG",
+              name:"Jenny"
+            },{
+              image:"fotos/peru/Jesenia Rodríguez.JPG",
+              name:"Jesenia"
+            },{
+              image:"fotos/peru/Johana Azañero.JPG",
+              name:"Johana"
+            },{
+              image:"fotos/peru/Johelyn Cordova.JPG",
+              name:"Johelyn"
+            },{
+              image:"fotos/peru/Pierina Acevedo.JPG",
+              name:"Pierina"
+            },{
+              image:"fotos/peru/Karina Prado.jpg",
+              name:"Karina"
+            },{
+              image:"fotos/peru/Karla Rios.jpg",
+              name:"Karla"
+            },{
+              image:"fotos/peru/Katherine Llacza.JPG",
+              name:"Katherine"
+            },{
+              image:"fotos/peru/Kiara Esquerra.JPG",
+              name:"Kiara"
+            },{
+              image:"fotos/peru/Lisbeth Balabarca.JPG",
+              name:"Lisbeth"
+            },{
+              image:"fotos/peru/Lizeth Lopez.JPG",
+              name:"Lizeth"
+            },{
+              image:"fotos/peru/Magaly Flores.jpg",
+              name:"Magaly"
+            },{
+              image:"fotos/peru/Maria Cecilia Limay.JPG",
+              name:"Cecilia"
+            },{
+              image:"fotos/peru/Maria Victoria Cardenas.jpg",
+              name:"Maria Victoria"
+            },{
+              image:"fotos/peru/Merlin Rupay.jpg",
+              name:"Stefanny"
+            },{
+              image:"fotos/peru/Milu Robles.JPG",
+              name:"Milu"
+            },{
+              image:"fotos/peru/Monica Ccencho.jpg",
+              name:"Monica"
+            },{
+              image:"fotos/peru/Nayra Pillihuaman.jpg",
+              name:"Nayra"
+            },{
+              image:"fotos/peru/Patricia Arakawa.JPG",
+              name:"Patricia"
+            },{
+              image:"fotos/peru/Reina Torrejón.JPG",
+              name:"Reina"
+            },{
+              image:"fotos/peru/Rita Ortiz.jpg",
+              name:"Rita"
+            },{
+              image:"fotos/peru/Sandra Azabache.JPG",
+              name:"Sandra"
+            },{
+              image:"fotos/peru/Sara Nolasco.JPG",
+              name:"Sara"
+            },{
+              image:"fotos/peru/Sarita Roman.jpg",
+              name:"Sarita"
+            },{
+              image:"fotos/peru/Tania Cuya.JPG",
+              name:"Tania"
+            },{
+              image:"fotos/peru/Vanessa Alvarado.jpg",
+              name:"Vanessa"
+            },{
+              image:"fotos/peru/Vanessa Vargas.JPG",
+              name:"Vanessa"
+            },{
+              image:"fotos/peru/Vickensi de la Cruz.jpg",
+              name:"Vickensi"
+            }];
